5b4035d2b0c92a1495e5a7f80687f39f4cc37315
Abhimanyu Sareen <avs7412@psu.edu>
2024-03-03 15:45:15 -0500

    Update file structure, remove old vanity js file

--- VanityAddress.js
+++ VanityAddress.js
@@ -1,25 +1,48 @@
-const TronWeb = require('tronweb');
+const { Worker } = require('worker_threads');
 const fs = require('fs');
+const os = require('os');
 
-const checkString = 'Abhi';
-const tronWeb = new TronWeb({
-    fullHost: 'http://52.53.189.99:8090' // or another Tron network endpoint
-});
+const numCPUs = os.cpus().length;
+let workers = [];
 
-async function findMatchingAddress() {
-    let matched = false;
-    while (!matched) {
-        let { address, privateKey } = await tronWeb.createAccount();
-		address = address.base58
-        // Assuming address is returned with a leading character that should be ignored
-        let tempAdd = address.substr(1);
-        if (tempAdd.startsWith(checkString)) {
-            matched = true;
+function createWorker() {
+    const worker = new Worker('./worker.js');
+    workers.push(worker);
+
+    worker.on('message', ({ address, privateKey, found }) => {
+        if (found) {
             console.log("\n\n ----------Found----------- \n\n");
             console.log(`${address} - ${privateKey}`);
             fs.writeFileSync(`${address}.txt`, `${address} - ${privateKey}`);
+            terminateAllWorkers(); // Terminate all workers once a match is found
         }
+    });
+
+    worker.on('error', (err) => {
+        console.error(err);
+        workers = workers.filter(w => w !== worker); // Remove the worker from the list on error
+    });
+
+    worker.on('exit', (code) => {
+        workers = workers.filter(w => w !== worker); // Remove the worker from the list on exit
+        if (workers.length === 0) {
+            // Optionally restart workers if needed, depending on your application's needs
+        }
+    });
+}
+
+function terminateAllWorkers() {
+    workers.forEach(worker => worker.terminate());
+    workers = []; // Clear the array of workers
+}
+
+function startWorkers() {
+    const numWorkers = Math.max(numCPUs - 1, 1); // Adjust based on preference and system capabilities
+    console.log(`Starting ${numWorkers} worker(s)...`);
+    for (let i = 0; i < numWorkers; i++) {
+        createWorker();
     }
 }
 
-findMatchingAddress();
+startWorkers();
+
